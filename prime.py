7086decb732fd3afe59ea62ba3431013f8085567
Ravikiran1617 <armoorravikiran1617@gmail.com>
2025-12-09 15:26:00 +0530

    pushing the changed code

--- prime.py
+++ prime.py
@@ -1,1 +1,30 @@
-i = int(input()) 
+num = int(input("Enter a number: "))
+
+if num > 1:
+    for i in range(2, num):
+        if num % i == 0:
+            print(num, "Hello welcome to prime number code")
+            break
+    else:
+        print(num, "is a PRIME NUMBER")
+else:
+    print(num, "is r")
+num = int(input("Enter "))
+if num > 1:
+    for i in range(2, num):
+        if num % i == 0:
+            print(num, "is not a prmber")
+            break
+    else:
+        print(num, "is a PRIME NUMBER")
+else:
+    print(num, "is not a prime number")
+
+
+#hello
+
+
+
+if True==True:
+    print('I am happy')
+#modified
